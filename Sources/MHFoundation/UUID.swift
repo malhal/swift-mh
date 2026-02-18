17c616e4cfc4a1010a6a1064720382972d6cf98c
Malcolm Hall <malhal@users.noreply.github.com>
2026-02-18 14:05:47 +0000

    Added UUID

---
+++ Sources/MHFoundation/UUID.swift
@@ -0,0 +1,11 @@
+//
+//  UUID.swift
+//  swift-mh
+//
+//  Created by Malcolm Hall on 16/02/2026.
+//
+import Foundation
+
+extension UUID {
+    public static let zero = UUID(uuid: (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)) // "00000000-0000-0000-0000-000000000000"
+}
